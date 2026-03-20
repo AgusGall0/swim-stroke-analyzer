c55c765083197c3e2dfb40caa5a2b0d452ae1953
Agustin Gallo <juanagustin09@gmail.com>
2026-03-20 18:09:10 -0300

    Apertura de archivo de video.mp4 con openCV

---
+++ apertura_v.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture('ejemplo.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    
+    cv.imshow('frame', frame)
+    if cv.waitKey(24) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
